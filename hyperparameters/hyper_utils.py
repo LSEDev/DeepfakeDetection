2dfe1b0a67326bcdd5865eb85b29bdd8335048ab
Greg <jupyter@greg-mobnet.us-central1-f.c.lse-st498.internal>
2020-07-12 12:12:44 +0000

    Remove val augmentation

---
+++ hyperparameters/hyper_utils.py
@@ -0,0 +1,189 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import Model
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, GlobalAveragePooling2D
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.layers import BatchNormalization
+import tensorflow.keras.backend as K
+import os
+import glob
+from tensorflow.keras.callbacks import *
+import warnings
+import logging
+from IPython.display import clear_output
+import sys
+
+#------------------------------------------------------------------------------------------------------------------------
+
+class PrintLR(tf.keras.callbacks.Callback):
+    '''Prints the LR to be used for the batch before the batch is fed into the network for training.'''
+    
+    def on_train_batch_begin(self, batch, logs=None):
+        LR = K.get_value(self.model.optimizer.lr)
+        if type(LR)==tf.python.keras.optimizer_v2.learning_rate_schedule.CosineDecay:
+            print("\nLearning rate:", LR.__call__(self.model.optimizer.iterations))
+        else:
+            print("\nLearning rate:", LR) 
+        print(K.get_value(self.model.optimizer.lr))
+        
+#------------------------------------------------------------------------------------------------------------------------
+        
+class CustomLearningRateScheduler(tf.keras.callbacks.Callback):
+    """Learning rate scheduler which sets the learning rate according to schedule.
+
+  Arguments:
+      schedule: a function that takes an epoch index
+          (integer, indexed from 0) and current learning rate
+          as inputs and returns a new learning rate as output (float).
+  """
+
+    def __init__(self, schedule):
+        super(CustomLearningRateScheduler, self).__init__()
+        self.schedule = schedule
+
+    def on_epoch_begin(self, epoch, logs=None):
+        if not hasattr(self.model.optimizer, "lr"):
+            raise ValueError('Optimizer must have a "lr" attribute.')
+        # Get the current learning rate from model's optimizer.
+        lr = float(K.get_value(self.model.optimizer.learning_rate))
+        # Call schedule function to get the scheduled learning rate.
+        scheduled_lr = self.schedule(epoch, lr)
+        # Set the value back to the optimizer before this epoch starts
+        K.set_value(self.model.optimizer.lr, scheduled_lr)
+        print("\nEpoch %05d: Learning rate is %6.4f." % (epoch, scheduled_lr))
+
+
+def lr_schedule(epoch, lr):
+    """Helper function to retrieve the scheduled learning rate based on epoch."""
+    if epoch < 1:
+        return lr/(6-epoch)
+    elif epoch == 5:
+        return lr
+    return lr
+
+
+#------------------------------------------------------------------------------------------------------------------------
+
+def cosine_decay_with_warmup(global_step,
+                             learning_rate_base,
+                             total_steps,
+                             warmup_learning_rate=0.0,
+                             warmup_steps=0,
+                             hold_base_rate_steps=0):
+    """Cosine decay schedule with warm up period.
+    Technique obtained from (cosine annealing rate):
+      Loshchilov and Hutter, SGDR: Stochastic Gradient Descent with Warm Restarts.
+      ICLR 2017. https://arxiv.org/abs/1608.03983
+    Linear growth from warmup rate to learning_rate_base for warmup_steps, transitioning to a cosine decay
+    schedule.
+    Arguments:
+        global_step {int} -- global step.
+        learning_rate_base {float} -- base learning rate.
+        total_steps {int} -- total number of training steps.
+    Keyword Arguments:
+        warmup_learning_rate {float} -- initial learning rate for warm up. (default: {0.0})
+        warmup_steps {int} -- number of warmup steps. (default: {0})
+        hold_base_rate_steps {int} -- Optional number of steps to hold base learning rate
+                                    before decaying. (default: {0})
+    Returns:
+      a float representing learning rate.
+    Raises:
+      ValueError: if warmup_learning_rate is larger than learning_rate_base,
+        or if warmup_steps is larger than total_steps.
+        
+    Adapted from Tony607 - https://gist.github.com/Tony607/28b8de1cd01a859e62cc77547d601fb5
+    """
+
+    if total_steps < warmup_steps:
+        raise ValueError('total_steps must be larger or equal to '
+                         'warmup_steps.')
+    learning_rate = 0.5 * learning_rate_base * (1 + np.cos(
+        np.pi *
+        (global_step - warmup_steps - hold_base_rate_steps
+         ) / float(total_steps - warmup_steps - hold_base_rate_steps)))
+    if hold_base_rate_steps > 0:
+        learning_rate = np.where(global_step > warmup_steps + hold_base_rate_steps,
+                                 learning_rate, learning_rate_base)
+    if warmup_steps > 0:
+        if learning_rate_base < warmup_learning_rate:
+            raise ValueError('learning_rate_base must be larger or equal to '
+                             'warmup_learning_rate.')
+        slope = (learning_rate_base - warmup_learning_rate) / warmup_steps
+        warmup_rate = slope * global_step + warmup_learning_rate
+        learning_rate = np.where(global_step < warmup_steps, warmup_rate,
+                                 learning_rate)
+    return np.where(global_step > total_steps, 0.0, learning_rate)
+
+
+class WarmUpCosineDecayScheduler(tf.keras.callbacks.Callback):
+    """Cosine decay with warmup learning rate scheduler
+    """
+
+    def __init__(self,
+                 learning_rate_base,
+                 total_steps,
+                 global_step_init=0,
+                 warmup_learning_rate=0.0,
+                 warmup_steps=0,
+                 hold_base_rate_steps=0,
+                 verbose=0):
+        """Constructor for cosine decay with warmup learning rate scheduler.
+    Arguments:
+        learning_rate_base {float} -- base learning rate.
+        total_steps {int} -- total number of training steps.
+    Keyword Arguments:
+        global_step_init {int} -- initial global step, e.g. from previous checkpoint.
+        warmup_learning_rate {float} -- initial learning rate for warm up. (default: {0.0})
+        warmup_steps {int} -- number of warmup steps. (default: {0})
+        hold_base_rate_steps {int} -- Optional number of steps to hold base learning rate
+                                    before decaying. (default: {0})
+        verbose {int} -- 0: quiet, 1: update messages. (default: {0})
+        """
+
+        super(WarmUpCosineDecayScheduler, self).__init__()
+        self.learning_rate_base = learning_rate_base
+        self.total_steps = total_steps
+        self.global_step = global_step_init
+        self.warmup_learning_rate = warmup_learning_rate
+        self.warmup_steps = warmup_steps
+        self.hold_base_rate_steps = hold_base_rate_steps
+        self.verbose = verbose
+        self.learning_rates = []
+
+    def on_batch_end(self, batch, logs=None):
+        self.global_step = self.global_step + 1
+        lr = K.get_value(self.model.optimizer.lr)
+        self.learning_rates.append(lr)
+
+    def on_batch_begin(self, batch, logs=None):
+        lr = cosine_decay_with_warmup(global_step=self.global_step,
+                                      learning_rate_base=self.learning_rate_base,
+                                      total_steps=self.total_steps,
+                                      warmup_learning_rate=self.warmup_learning_rate,
+                                      warmup_steps=self.warmup_steps,
+                                      hold_base_rate_steps=self.hold_base_rate_steps)
+        K.set_value(self.model.optimizer.lr, lr)
+#         if self.verbose > 0:
+#             print('\nBatch %05d: setting learning '
+#                   'rate to %s.' % (self.global_step + 1, lr))
+            
+    def on_batch_begin(self, batch, logs=None):
+        lr = cosine_decay_with_warmup(global_step=self.global_step,
+                                      learning_rate_base=self.learning_rate_base,
+                                      total_steps=self.total_steps,
+                                      warmup_learning_rate=self.warmup_learning_rate,
+                                      warmup_steps=self.warmup_steps,
+                                      hold_base_rate_steps=self.hold_base_rate_steps)
+        print('\nBatch %05d: setting learning '
+              'rate to %s.' % (self.global_step + 1, lr))
+
+
+#------------------------------------------------------------------------------------------------------------------------
+
+
+    
+    
